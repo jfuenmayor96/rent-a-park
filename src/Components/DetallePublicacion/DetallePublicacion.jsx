cc123d8a7bea74be95926f22fa0bc04a6344e653
Julio Fuenmayor <13-10488@usb.ve>
2019-04-29 12:11:52 -0400

    se obtiene la información de la publicación a mostrar

--- src/Components/DetallePublicacion/DetallePublicacion.jsx
+++ src/Components/DetallePublicacion/DetallePublicacion.jsx
@@ -1,7 +1,40 @@
-import React, { Component } from "react";
+import React, { setState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import "./DetallePublicacion.scss";
 
-export default class DetallePublicacion extends Component {
-  render() {
-    return <div />;
+const publicaciones = [
+  {
+    id: 1,
+    ubicacion: "El Hatillo",
+    precio: "2000bs"
+  },
+  {
+    id: 2,
+    ubicacion: "Los Palos Grandes",
+    precio: "2500bs"
+  },
+  {
+    id: 3,
+    ubicacion: "Oripoto",
+    precio: "1500bs"
+  },
+  {
+    id: 4,
+    ubicacion: "El Cafetal",
+    precio: "1900bs"
+  }
+];
+
+function DetallePublicacion(props) {
+  let publicacion = publicaciones.filter(
+    _p => _p.id === parseInt(props.match.params.id, 10)
+  )[0];
+
+  if (publicacion) {
+    return <div>Informacion</div>;
+  } else {
+    return <div>Publicación inválida</div>;
   }
 }
+
+export default withRouter(DetallePublicacion);
