42a3b6d9c03ec3b9d31faee28a442a373e0f2bca
Julio Fuenmayor <13-10488@usb.ve>
2019-04-29 09:39:56 -0400

    se agrega switch faltante al router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Listado from "./Components/Listado/Listado";
 import DetallePublicacion from "./Components/DetallePublicacion/DetallePublicacion";
@@ -8,9 +8,11 @@ import DetallePublicacion from "./Components/DetallePublicacion/DetallePublicaci
 function App() {
   return (
     <Router>
-      <Route path="/detalle-publicacion/:id" component={DetallePublicacion} />
-      <Route path="/listado" component={Listado} />
-      <Route path="/home" component={Home} />
+      <Switch>
+        <Route path="/detalle-publicacion/:id" component={DetallePublicacion} />
+        <Route path="/listado" component={Listado} />
+        <Route path="/home" component={Home} />
+      </Switch>
     </Router>
   );
 }
