e7c48efbc945feb1be2ab872fb02c214baf2a2b0
Julio Fuenmayor <13-10488@usb.ve>
2019-04-29 11:36:34 -0400

    se termina el modal de búsqueda

--- src/Components/Home/ModalBusqueda.jsx
+++ src/Components/Home/ModalBusqueda.jsx
@@ -1,20 +1,44 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import {
+  Col,
   Modal,
   ModalHeader,
   ModalBody,
   ModalFooter,
   Button,
-  InputGroup,
   Input
 } from "reactstrap";
 
-export default function ModalBusqueda(props) {
+function ModalBusqueda(props) {
   return (
     <Modal isOpen={props.isOpen} toggle={() => props.toggleModal()}>
       <ModalHeader toggle={() => props.toggleModal()}>
         Buscar un puesto
       </ModalHeader>
+      <ModalBody>
+        <Input type="select">
+          <option value="hatillo">El Hatillo</option>
+          <option value="mercedes">Las Mercedes</option>
+          <option value="oripoto">Oripoto</option>
+          <option value="cafetal">El Cafetal</option>
+          <option value="palos_grandes">Los Palos Grandes</option>
+        </Input>
+      </ModalBody>
+      <ModalFooter>
+        <Col xs={12} sm={12} md={12} lg={12} className="text-center">
+          <Button
+            color="primary"
+            onClick={() => {
+              props.history.push("/listado");
+            }}
+          >
+            Buscar
+          </Button>
+        </Col>
+      </ModalFooter>
     </Modal>
   );
 }
+
+export default withRouter(ModalBusqueda);
